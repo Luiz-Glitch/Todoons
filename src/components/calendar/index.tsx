2c9c5b5e5176ff3d50d8c433412452a9921beef1
Adryan1928 <adryanoliveira1928@gmail.com>
2023-12-31 10:53:09 -0300

    Criei a lógica dos dias para o calendário(Falta Fevereiro)

--- src/components/calendar/index.tsx
+++ src/components/calendar/index.tsx
@@ -1,23 +1,72 @@
 import React from "react";
-import { Container, TextNotToday, TextToday } from "./style";
-import { View } from "react-native";
+import { Container, ContainerDay, TextMonth, TextNotToday, TextToday } from "./style";
+import { View, Text } from "react-native";
+
+// Falta criar a lógica para Fevereiro, mas estou com preguiça😭 (Depois eu faço😊)
 
 export function Calendar(){
-
     const today = new Date().getDate()
-    const listDay = [today-2, today-1, today, today+1, today+2]
-    console.log(listDay)
+    const monthNumber = new Date().getMonth()
+    // const today = 1
+    // const monthNumber = 11
+    
+    const monthList = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+
+    const month = monthList[monthNumber]
+
+    const month31 = [0,2,4,6,7,9,11]
+    let is31 = false
+    let isPast31 = false
+    let listDay = [today-2, today-1, today, today+1, today+2]
+
+    if (month31.includes(monthNumber))
+        is31 = true
+
+    if (monthNumber <= 7 && monthNumber % 2 == 1 || monthNumber > 7 && monthNumber % 2 == 0 || monthNumber == 0)
+        isPast31 = true
+
+    switch (today) {
+        case 1:
+            if (isPast31)
+                listDay = [30, 31, today, today+1, today+2]
+            else
+                listDay = [29, 30, today, today+1, today+2]
+            break
+        case 2:
+            if (isPast31)
+                listDay = [31, 1, today, today+1, today+2]
+            else
+                listDay = [30, 1, today, today+1, today+2]
+            break
+        case 29:
+            if (!is31)
+                listDay = [today-2, today-1, today, today+1, 1]
+            break
+        case 30:
+            if (is31)
+                listDay = [today-2, today-1, today, today+1, 1]
+            else
+                listDay = [today-2, today-1, today, 1, 2]
+            break
+        case 31:
+            listDay = [today-2, today-1, today, 1, 2]
+            break
+    }
+
     return (
         <Container>
-            { listDay.map((day, index) => (
-                <View key={index}>
-                    {index == 2?
-                    <TextToday>{day}</TextToday>
-                    :
-                    <TextNotToday>{day}</TextNotToday>
-                    }
-                </View>
-            ))}
+            <TextMonth>{month}</TextMonth>
+            <ContainerDay>
+                { listDay.map((day, index) => (
+                    <View key={index}>
+                        {index == 2?
+                        <TextToday>{day}</TextToday>
+                        :
+                        <TextNotToday>{day}</TextNotToday>
+                        }
+                    </View>
+                ))}
+            </ContainerDay>
         </Container>
     )
 }
--- src/components/calendar/style.ts
+++ src/components/calendar/style.ts
@@ -1,24 +1,38 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-    flex-direction: row;
-    gap: 10px;
-    justify-content: center;
+    gap: 8px;
+    align-items: center;
     margin-top: 16px;
 `
 
+export const ContainerDay = styled.View`
+    flex-direction: row;
+    gap: 8px;
+    justify-content: center;
+`
+
+export const TextMonth = styled.Text`
+    font-weight: 700;
+    font-size: ${({theme}) => theme.fontSize.sm};
+`
+
 export const TextToday = styled.Text`
+    width: 42px;
+    text-align: center;
     color: ${({theme}) => theme.colors.gray[50]};
     background-color: ${({theme}) => theme.colors.gray[700]};
     font-size: ${({theme}) => theme.fontSize.md};
-    padding: 4px 8px;
+    padding: 4px 0px;
     border-radius: 8px;
 `
 
 export const TextNotToday = styled.Text`
+    width: 42px;
+    text-align: center;
     color: ${({theme}) => theme.colors.gray[50]};
     background-color: ${({theme}) => theme.colors.gray[200]};
     font-size: ${({theme}) => theme.fontSize.md};
-    padding: 4px 8px;
+    padding: 4px 0px;
     border-radius: 8px;
 `
