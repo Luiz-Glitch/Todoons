2a4eda89f9bd09843ce38ec700ce41823083266c
Adryan1928 <adryanoliveira1928@gmail.com>
2024-01-08 09:23:02 -0300

    Add react magic motion

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,24 +4,27 @@ import { Header } from '../../components/header';
 import { Calendar } from '../../components/calendar';
 import { Container, ContainerButton, ContainerHeaderCalendar } from './style';
 import { Button } from '../../components/button';
+import { MagicMotion } from 'react-magic-motion';
 
 export function Home() {
   return (
-    <Container>
-      <ScrollView>
-        <StatusBar
-          hidden = {false}
-          backgroundColor = "#736CED"
-          translucent = {false}
-        />
-        <ContainerHeaderCalendar>
-          <Header />
-          <Calendar/>
-        </ContainerHeaderCalendar>
-      </ScrollView>
-      <ContainerButton>
-        <Button label='Criar tarefa'/>
-      </ContainerButton>
-    </Container>
+    <MagicMotion>
+      <Container>
+        <ScrollView>
+          <StatusBar
+            hidden = {false}
+            backgroundColor = "#736CED"
+            translucent = {false}
+          />
+          <ContainerHeaderCalendar>
+            <Header />
+            <Calendar/>
+          </ContainerHeaderCalendar>
+        </ScrollView>
+        <ContainerButton>
+          <Button label='Criar tarefa'/>
+        </ContainerButton>
+      </Container>
+    </MagicMotion>
   );
 }
