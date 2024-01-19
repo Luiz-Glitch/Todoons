13dbc2139659150956a4efd9716ede50c8d3753f
Luiz Bessa <lbessaregis@gmail.com>
2024-01-19 14:31:48 -0300

    Fix typos in components and remove unused style

--- src/components/atoms/DateRangeInput/styles.ts
+++ src/components/atoms/DateRangeInput/styles.ts
@@ -17,24 +17,6 @@ export const Placeholder = styled.Text`
   color: ${theme.colors.gray[400]};
 `;
 
-export const CalendarContainer = styled.View<{
-  relativeHeight: number;
-  relativeWidth: number;
-  width: number;
-  showShadow: boolean;
-}>`
-  min-width: 260px;
-  width: 80%;
-  height: 100%;
-  display: flex;
-  align-self: center;
-  position: absolute;
-  margin: 12px 8px 12px 8px;
-  z-index: 2;
-  border-radius: 0px 0px 8px 8px;
-  background-color: ${theme.colors.primary[500]};
-`;
-
 export const Overlay = styled.View`
   flex: 1;
   background-color: rgba(0, 0, 0, 0.5);
