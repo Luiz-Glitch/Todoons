c669266eb70fbbbcb69471a53e840fcc3aa117da
Adryan1928 <adryanoliveira1928@gmail.com>
2024-02-06 14:54:53 -0300

    A barra de pesquisa está concluída

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -10,11 +10,39 @@ import { Task } from '../../components/atoms/task';
 import { SearchBar } from '../../components/molecules/SearchBar';
 import { useMainContext } from '../../hooks/useMainContext';
 import { RootStackParamsList } from '../../navigators/RootStackParams';
+import { useMemo, useState } from 'react';
+import { TaskProps } from '../../contexts/main';
+
+const removeAccents = (str: string) => str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+
+const filterTaskByTitleOrPriority = (task: TaskProps, query:String) => {
+  let queryPriority = query.split(':')
+  return (
+    removeAccents(task.title.toLowerCase()).includes(
+      removeAccents(query.toLowerCase()),
+    ) ||
+    task.priority?.includes(
+      queryPriority[1]? queryPriority[1].toUpperCase(): '0'
+    )
+  );
+};
 
 type createScreenProp = NativeStackNavigationProp<RootStackParamsList, 'CreateTask'>;
 export function HomeScreen() {
+  const [query, setQuery] = useState('')
   const navigation = useNavigation<createScreenProp>();
   const { tasks } = useMainContext();
+
+  const tasksFilter = useMemo(() => {
+    let filteredTasks:TaskProps[] = tasks
+    if (query) {
+      filteredTasks = filteredTasks.filter((task) => {
+        return filterTaskByTitleOrPriority(task, query)
+      })
+    }
+
+    return filteredTasks
+  }, [tasks, query])
 
   return (
     <Container>
@@ -22,10 +50,10 @@ export function HomeScreen() {
         <ContainerHeaderCalendar>
           <Header />
           <Calendar />
-          <SearchBar placeholder="Search Tasks" />
+          <SearchBar placeholder="Procurar Tarefas" onChangeText={(text) => setQuery(text)} />
         </ContainerHeaderCalendar>
         <ContainerTask>
-          {tasks.map((task) => (
+          {tasksFilter.map((task) => (
             <Task key={task.id} task={task} />
           ))}
         </ContainerTask>
